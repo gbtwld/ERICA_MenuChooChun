a16597ec08bef2b8cf1f880d17dfe744b1376fba
dujaejeong <gbtwld@gmail.com>
2019-12-15 19:19:19 +0900

    일미닭갈비 추가

--- food.cpp
+++ food.cpp
@@ -245,6 +245,25 @@ void Vicepresident() {
     menu["16"] = vec;
 }
 
+void Oneme() {
+	vector<pair<string, int>> vec;
+	vec.push_back({ "닭갈비(중)", 14000 });
+	vec.push_back({ "닭갈비(대)", 18000 });
+	vec.push_back({ "닭갈비(특대)", 22000 });
+	vec.push_back({ "쭈꾸미(중)", 15000 });
+	vec.push_back({ "쭈꾸미(대)", 19000 });
+	vec.push_back({ "쭈꾸미(특대)", 23000 });
+	vec.push_back({ "닭-삼(중)", 16000 });
+	vec.push_back({ "닭-삼(대)", 20000 });
+	vec.push_back({ "닭-삼(특대)", 24000 });
+	vec.push_back({ "쭈-닭(삼)(중)", 16000 });
+	vec.push_back({ "쭈-닭(삼)(대)", 20000 });
+	vec.push_back({ "쭈-닭(삼)(특대)", 24000 });
+
+	menu["일미닭갈비"] = vec;
+    menu["17"] = vec;
+}
+
 void init() {
     //위에서 지정한 식당 이름 적기
     alchon(); //알촌
@@ -278,7 +297,7 @@ int main() {
         }
         cin.ignore();
         init();
-        cout << "\n1. 행복한 짬뽕\n2. 알촌\n3. 쿡스빌\n4. 브라더 스테이크\n5. 명동찌개마을\n6. 유메식당\n7. 미하루식당\n8. 밀플랜비\n9. 이서식당\n10. 한그릇\n11. 포지타노\n12. 피자리아\n""13. 디델리\n14. 도스마스 \n15. 찌개찌개\n16. 부대통령\n\n식당 입력: ";
+        cout << "\n1. 행복한 짬뽕\n2. 알촌\n3. 쿡스빌\n4. 브라더 스테이크\n5. 명동찌개마을\n6. 유메식당\n7. 미하루식당\n8. 밀플랜비\n9. 이서식당\n10. 한그릇\n11. 포지타노\n12. 피자리아\n""13. 디델리\n14. 도스마스 \n15. 찌개찌개\n16. 부대통령\n17. 일미닭갈비\n\n식당 입력: ";
         string store;
         getline(cin, store);
         store.erase(remove(store.begin(), store.end(), ' ' ), store.end());
